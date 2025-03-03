bb16e549412c2fee36eaa25a7a5831d305b6160e
Carlos Eduardo <edugah1809@gmail.com>
2025-03-03 16:59:53 -0300

    Atualização, adicionei sistema de pontos simples (sem banco de dados

--- Rock-paper-scissors/script.js
+++ Rock-paper-scissors/script.js
@@ -3,6 +3,13 @@ const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const escolha = document.getElementById("escolha");
 const enviar = document.getElementById("jogar");
+var playerChoosen = ""
+var playerPointElement = document.getElementById('playerPoints');
+var pcPointElement = document.getElementById('pcPoints');
+var _playerPoint = 0;
+var _pcPoint = 0;
+
+
 
 function animar(botao) {
     botao.style.transform = "scale(1.1)"; 
@@ -30,7 +37,7 @@ scissors.addEventListener("click", () => {
     playerChoosen = 'Tesoura'
 });    
 enviar.addEventListener("click", () => {
-    if (escolha.textContent != 'Escolha um'){
+    if (escolha.textContent != ""){
         var IA = Math.floor(Math.random() * 3) + 1;
         if (IA == 1){
             window.alert(`Escolhi Papel e você ${playerChoosen}`)
@@ -38,8 +45,12 @@ enviar.addEventListener("click", () => {
                 window.alert('Empate!')
             }else if (playerChoosen == 'Pedra'){
                 window.alert('Eu ganhei!')
+                _pcPoint++
+                pcPointElement.textContent = _pcPoint
             }else{
                 window.alert('Você Ganhou')
+                _playerPoint++
+                playerPointElement.textContent = _playerPoint
             }
         }else if (IA == 2){
             window.alert(`Escolhi Pedra você ${playerChoosen}`)
@@ -47,8 +58,12 @@ enviar.addEventListener("click", () => {
                 window.alert('Empate!')
             }else if (playerChoosen == 'Tesoura'){
                 window.alert('Eu ganhei!')
+                _pcPoint++
+                pcPointElement.textContent = _pcPoint
             }else{
                 window.alert('Você Ganhou')
+                _playerPoint++
+                playerPointElement.textContent = _playerPoint
             }
         }else{
             window.alert(`Escolhi Tesoura você ${playerChoosen}`)
@@ -56,8 +71,12 @@ enviar.addEventListener("click", () => {
                 window.alert('Empate!')
             }else if (playerChoosen == 'Papel'){
                 window.alert('Eu ganhei!')
+                _pcPoint++
+                pcPointElement.textContent = _pcPoint
             }else{
                 window.alert('Você Ganhou')
+                _playerPoint++
+                playerPointElement.textContent = _playerPoint
             }
         }
     }else{
